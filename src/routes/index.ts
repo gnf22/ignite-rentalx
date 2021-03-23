0c34efd81f9ba5bfb5142f375d9422ea792344b6
Gustavo <gnferreira2000@gmail.com>
2021-03-23 04:40:28 -0300

    create: POST/users

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,8 +2,10 @@ import { Router } from 'express';
 
 import { categoriesRoutes } from './categories.routes';
 import { specificationsRoutes } from './specification.routes';
+import { usersRoutes } from './user.routes';
 
 export const router = Router();
 
 router.use('/categories', categoriesRoutes);
 router.use('/specifications', specificationsRoutes);
+router.use('/users', usersRoutes);
