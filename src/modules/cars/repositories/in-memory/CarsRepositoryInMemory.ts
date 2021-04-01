06b5e004e871e1f6098cc09cc05b58ae6e8afa38
Gustavo <gnferreira2000@gmail.com>
2021-04-01 03:30:53 -0300

    add: car usecase and anothers

--- src/modules/cars/repositories/in-memory/CarsRepositoryInMemory.ts
+++ src/modules/cars/repositories/in-memory/CarsRepositoryInMemory.ts
@@ -14,7 +14,7 @@ export class CarsRepositoryInMemory implements ICarsRepository {
     fine_amount,
     license_plate,
     name,
-  }: ICreateCarDTO): Promise<void> {
+  }: ICreateCarDTO): Promise<Car> {
     const car = new Car();
 
     Object.assign(car, {
@@ -28,5 +28,13 @@ export class CarsRepositoryInMemory implements ICarsRepository {
     });
 
     this.cars.push(car);
+
+    return car;
+  }
+
+  async findByLicensePLate(license_plate: string): Promise<Car> {
+    const car = this.cars.find(car => car.license_plate === license_plate);
+
+    return car;
   }
 }
